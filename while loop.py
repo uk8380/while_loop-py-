6e6f51f7c9b5a1c029a71125c6eaf43c7e770a6f
mannepalli uday kumar <65896248+uk8380@users.noreply.github.com>
2021-06-19 14:05:28 +0530

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,24 @@
+#while loop
+a=10
+while(a<20):
+    print(a)
+    a+=1
+#continue
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+#break
+  i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+#infinity loop
+  while(o<0):
+      print("lobve")
+        
+        
